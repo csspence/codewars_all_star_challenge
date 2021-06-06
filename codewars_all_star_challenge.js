73b6634f1c5fb4eb1ff5dbff67da43a030859a6e
Crew Spence <csspence123@gmail.com>
2021-06-06 06:15:21 -0500

    Wrote working solution.

--- codewars_all_star_challenge.js
+++ codewars_all_star_challenge.js
@@ -16,6 +16,15 @@ sumPPG(iverson, jordan); // => 31.4
 
 */
 
-function sumPPG(playerOne, playerTwo){
+function NBAplayer(name, team, ppg){
+  this.name=name;
+  this.team=team;
+  this.ppg=ppg;
+}
 
+var iverson = new NBAplayer("Iverson", "76ers", 11.2);
+var jordan = new NBAplayer("Jordan", "bulls", 20.2);
+
+function sumPPG(playerOne, playerTwo){
+  return playerOne.ppg + playerTwo.ppg;
 }
